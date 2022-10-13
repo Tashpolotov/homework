e3be01698ce56e357f71deaf0e3569994a80c54b
Tashpolotov <112759983+Tashpolotov@users.noreply.github.com>
2022-10-13 14:41:49 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,18 @@
-import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws amountException {
         Account account = new Account();
-        try {
-            account.deposid(20000.0);
-        } catch (amountException a) {
-            a.printStackTrace();
-        }
-        Scanner scanner = new Scanner(System.in);
+        account.Deposit(20000.0);
+
         while (true) {
             try {
-                System.out.println("Napiwite summu vyvoda: ");
-                account.withDraw(scanner.nextDouble());
-
-                System.out.println("Вы сняли со счета: "  + scanner.nextDouble() + "som: " +
-                        "ostatok na s4etu: " + account.getAmount());
+                account.withDraw(6000);
             } catch (amountException amountException) {
                 System.out.println(amountException.getMessage());
-                try {
-                    account.withDraw( amountException.getRemainingAmount());
-                } catch (amountException b) {
-                    b.printStackTrace();
-                }
-                break;
+                    break;
+            }
+                 }
 
             }
         }
-
-    }
-}
