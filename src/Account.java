4c5c23025d417a252af14e3438c3ee5a303c5bf5
Tashpolotov <112759983+Tashpolotov@users.noreply.github.com>
2022-10-13 14:42:41 +0600

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -1,28 +1,30 @@
 import javax.naming.LimitExceededException;
 
 public class Account {
-    private Double amount = 0.0;
+    private double amount;
 
-    public Double getAmount() {
+    public double getAmount() {
         return amount;
-
     }
 
-    public void deposid(Double som) throws amountException {
+    public void Deposit(double som)throws amountException {
         amount += som;
         System.out.println("Вы пополнили счет на: " + (double) getAmount() + " Сом");
     }
 
-    public void withDraw(Double som) throws amountException {
-        if (som > amount) {
-            throw new amountException("Операция не доступна: ", amount);
-        } else {
-            amount -= som;
-            System.out.println("Вы сняли со счета: "  + "som: " +
-                    "ostatok na s4etu: " + amount);
-        }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 
+    public void withDraw(int sum) throws amountException {
+        if (sum > amount ) {
+            throw new amountException ("Операция недоступна, недостаточно средств на счету: " + getAmount() +
+                    " :а также вы можете снять 2000 сом: " +
+                    "Вы сняли :" + getAmount() + "Остаток на счете 0.0 :");}
+        else {
+            amount -= sum;
+            System.out.println("Вы сняли: " + sum + " Сом" + " Остаток на счету: "
+                    + amount);
+        }
     }
 }
-
-
